4992edfe7829d7b7173c1039f467fe8de5e6a14a
Dan Leslie <dan@ironoxide.ca>
2012-11-04 12:56:30 -0800

    Added package.el headers

--- seclusion-mode.el
+++ seclusion-mode.el
@@ -1,3 +1,13 @@
+;;; seclusion-mode.el --- Not actually a major/minor mode, but a frame configuration for total seclusion. A new frame is opened on the current buffer with as little clutter as possible and in fullscreen.
+
+;; Copyright 2012 Daniel Leslie
+;; Author: Daniel Leslie
+;; URL: http://github.com/dleslie/seclusion-mode
+;; Version: 1.0.0
+
+;; Licensed under the GPL3
+;; A copy of the license can be found at the above URL
+
 (defun seclusion-mode (command)
       "Window mode to hide all the clutter and help you focus."
       (interactive "CEnter command to run:")
@@ -8,8 +18,7 @@
           (call-interactively command)
           (error (princ (format "Error occurred in executing Theatre command: %s" err))))
       ;; Go full screen (Emacs 24+)
-      (set-frame-parameter nil 'fullscreen
-       (when (not (frame-parameter nil 'fullscreen)) 'fullboth))
+      (set-frame-parameter nil 'fullscreen (when (not (frame-parameter nil 'fullscreen)) 'fullboth))
       ;; Make some nice fat fringes
       (let* ((dim (window-pixel-edges))
              (size (/ (- (nth 2 dim) (nth 0 dim)) 4)))
@@ -24,3 +33,5 @@
                                      (overlay-arrow . nil))))
 
 (provide 'seclusion-mode)
+
+;;; seclusion-mode.el ends here
