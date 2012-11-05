55fcebc760c31b94bd89478b49cd623af54a8f0c
Dan Leslie <dan@ironoxide.ca>
2012-11-04 19:44:27 -0800

    Added customization options

--- seclusion-mode.el
+++ seclusion-mode.el
@@ -3,34 +3,85 @@
 ;; Copyright 2012 Daniel Leslie
 ;; Author: Daniel Leslie
 ;; URL: http://github.com/dleslie/seclusion-mode
-;; Version: 1.0.0
+;; Version: 1.1.0
 
 ;; Licensed under the GPL3
 ;; A copy of the license can be found at the above URL
+
+(defgroup seclusion-mode
+  nil "Seclusion Mode")
+
+(defcustom 
+  seclusion-hide-minibuffer t
+  "Should the minibuffer be hidden?"
+  :type 'boolean
+  :group 'seclusion-mode)
+
+(defcustom
+  seclusion-hide-modeline t
+  "Should the minibuffer be hidden?"
+  :type 'boolean
+  :group 'seclusion-mode)
+
+(defcustom
+  seclusion-hide-menu t
+  "Should the menubar be hidden?"
+  :type 'boolean
+  :group 'seclusion-mode)
+
+(defcustom
+  seclusion-hide-toolbar t
+  "Should the toolbar be hidden?"
+  :type 'boolean
+  :group 'seclusion-mode)
+
+(defcustom
+  seclusion-hide-fringe-clutter t
+  "Should the fringe clutter be hidden?"
+  :type 'boolean
+  :group 'seclusion-mode)
+
+(defcustom
+  seclusion-fringe-factor 4
+  "The dividend of the 1/n of the screen the fringes consume."
+  :type 'integer
+  :group 'seclusion-mode)
 
 (defun seclusion-mode (command)
       "Window mode to hide all the clutter and help you focus."
       (interactive "CEnter command to run:")
       ;; New window
-      (select-frame (new-frame '((menu-bar-lines . 0) (minibuffer . nil) (toolbar . nil))))
+      (let ((options '()))
+        (if seclusion-hide-menu
+            (setq options (cons '(menu-bar-lines . 0) options)))
+        (if seclusion-hide-minibuffer
+            (setq options (cons '(minibuffer . nil) options)))
+        (if seclusion-hide-toolbar
+            (setq options (cons '(toolbar . nil) options)))
+        (select-frame (new-frame options)))
       ;; Execute the user command, but don't trip and fall on it
       (condition-case err
           (call-interactively command)
-          (error (princ (format "Error occurred in executing Theatre command: %s" err))))
+          (error (princ (format "Error occurred in executing client command: %s" err))))
       ;; Go full screen (Emacs 24+)
       (set-frame-parameter nil 'fullscreen (when (not (frame-parameter nil 'fullscreen)) 'fullboth))
       ;; Make some nice fat fringes
       (let* ((dim (window-pixel-edges))
-             (size (/ (- (nth 2 dim) (nth 0 dim)) 4)))
+             (size (/ (- (nth 2 dim) (nth 0 dim)) seclusion-fringe-factor)))
         (set-window-fringes nil size size))
       ;; Kill the mode line
-      (setq mode-line-format nil)
+      (if seclusion-hide-modeline (setq mode-line-format nil))
       ;; Kill the fringe clutter
-      (setq fringe-indicator-alist '((truncation . nil)
-                                     (continuation . nil)
-                                     (up . nil) (down . nil) (top . nil) (bottom . nil) (top-bottom .nil)
-                                     (empty-line . nil)
-                                     (overlay-arrow . nil))))
+      (if seclusion-hide-fringe-clutter
+          (setq fringe-indicator-alist '((truncation . nil)
+                                         (continuation . nil)
+                                         (up . nil) 
+                                         (down . nil) 
+                                         (top . nil) 
+                                         (bottom . nil) 
+                                         (top-bottom .nil)
+                                         (empty-line . nil)
+                                         (overlay-arrow . nil)))))
 
 (provide 'seclusion-mode)
 
